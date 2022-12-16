786b5f533e58ed1a13f849e5bf60ae2c1f0ae961
dora-jpg <theodorapanteliou@gmail.com>
2022-12-16 11:52:26 +0200

    Changed image path in params.py

--- src/params.py
+++ src/params.py
@@ -17,8 +17,8 @@ BANNERS = [
 
 TITLE = 'RavenAI GameDev Toolkit'
 
-LOGO = "../img/fav2.png"
-LOGO_BANNER = "../img/RavenAI_banner_2.png"
+LOGO = "./img/fav2.png"
+LOGO_BANNER = "./img/RavenAI_banner_2.png"
 API_KEY = "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"
 
 GENRE_OPTIONS = ['Action', 'Adventure', 'Fighting', 'Platform', 'Puzzle', 'Racing', 'Base-Building', 'Survival', 'Role-Playing', 'Shooter', 'Simulation', 'Sports', 'Strategy']
